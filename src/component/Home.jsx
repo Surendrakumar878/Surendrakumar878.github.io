661d1c9cd79b6a2276d227fd7802727001526431
Surendra kumar <59563905+Surendrakumar878@users.noreply.github.com>
2022-10-18 20:28:17 +0530

    h

---
+++ src/component/Home.jsx
@@ -0,0 +1,65 @@
+import { Box, Center, Flex, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <Box>
+     <Heading align="center">Hi 👋, I'm Surendra Kumar</Heading>
+<Heading fontSize={20} align="center">Aspiring Fullstack (MERN) Developer</Heading>
+
+
+
+<Center>
+<Heading align="left">Connect with me:</Heading>
+</Center><Center>
+<Flex p={2} minWidth='max-content' alignItems='center' gap='2'>
+<a href="https://dev.to/surendrakumar878" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/devto.svg" alt="surendrakumar878" height="30" width="40" /></a>
+<a href="https://twitter.com/surendr72033426" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="surendr72033426" height="30" width="40" /></a>
+<a href="https://linkedin.com/in/surendra-kumar-8140aa19b" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="surendra-kumar-8140aa19b" height="30" width="40" /></a>
+<a href="https://codesandbox.com/surendrakumar878" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/codesandbox.svg" alt="surendrakumar878" height="30" width="40" /></a>
+<a href="https://medium.com/@surendramarshkole78" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/medium.svg" alt="@surendramarshkole78" height="30" width="40" /></a>
+<a href="https://www.youtube.com/c/surendra kumar" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/youtube.svg" alt="surendra kumar" height="30" width="40" /></a>
+<a href="https://www.hackerrank.com/surendramarshko1" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/hackerrank.svg" alt="surendramarshko1" height="30" width="40" /></a>
+<a href="https://discord.gg/sure#5175" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/discord.svg" alt="sure#5175" height="30" width="40" /></a>
+</Flex>
+</Center>
+<Center>
+<Heading m="auto" align="left">Languages and Tools:</Heading></Center>
+<Center>
+<Flex m="auto" p={2} minWidth='max-content' alignItems='center' gap='2'> <a href="https://babeljs.io/" target="_blank" rel="noreferrer"> 
+<img src="https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg" alt="babel" width="40" height="40"/> </a> 
+<a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+     <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a> 
+     <a href="https://www.djangoproject.com/" target="_blank" rel="noreferrer"> 
+     <img src="https://cdn.worldvectorlogo.com/logos/django.svg" alt="django" width="40" height="40"/> </a>
+      <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> 
+      <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> 
+      </a> <a href="https://heroku.com" target="_blank" rel="noreferrer"> 
+      <img src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg" alt="heroku" width="40" height="40"/> </a>
+
+       <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> 
+       <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> 
+       <a href="https://www.java.com" target="_blank" rel="noreferrer"> 
+       <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="40" height="40"/> </a>
+        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">
+         <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a>
+          <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+             <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a> 
+         <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+             <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a> 
+         <a href="https://reactnative.dev/" target="_blank" rel="noreferrer"> 
+<img src="https://reactnative.dev/img/header_logo.svg" alt="reactnative" width="40" height="40"/> </a> 
+<a href="https://redux.js.org" target="_blank" rel="noreferrer"> 
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" alt="redux" width="40" height="40"/> </a> </Flex></Center>
+
+<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=surendrakumar878&show_icons=true&locale=en&layout=compact" alt="surendrakumar878" /></p>
+
+<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=surendrakumar878&show_icons=true&locale=en" alt="surendrakumar878" /></p>
+
+<p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=surendrakumar878&" alt="surendrakumar878" /></p>
+
+    </Box>
+  )
+}
+
+export default Home
